73b0e7016d8d5db09f01aa6a0225be1c956d95b5
Ssuncreature <sunniiflowo.dawn@gmail.com>
2026-09-13 18:34:17 +1200

    removed some out of date stuff

--- scripts/id.js
+++ scripts/id.js
@@ -12,14 +12,3 @@ function IDcompare(){
    */
 
     /** READ THE FILES GOES HERE */
-
-  const memberByID = {}
-    for (const member of clanCats) {
-      memberByID[member.ID] = member;
-    }
-  missing_cats = {}
-
-    if (member.ID (not in clan.clan_cats)) {
-      missing_cats.append(member.ID)
-    }
-}
